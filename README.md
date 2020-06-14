150e13633fbaebc061dca06bf419b14acf449af5
mad1ost <mad1ost@mail.ru>
2020-06-14 15:35:49 +0300

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+# Scroll to top bar
+
+Add scroll to top bar when hovering over a scrollbar of the page.
+
+Click on the "scroll to top bar" and you will be moved to the top of the page; click on it again and you will be moved to the previous scroll position. See also extension preferences.
---
+++ scroll-to-top-bar.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const scrollBar = createScrollBar();
+document.body.append(scrollBar);
+
+window.addEventListener('mouseover', (event) => {
+  if (event.target !== document.documentElement) return;
+  if (event.clientX >= document.documentElement.clientWidth) {
+    scrollBar.classList.add('over');
+  }
+});
+
+scrollBar.addEventListener('mouseleave', (event) => {
+  scrollBar.classList.remove('over');
+});
+
+let savedPageYOffset = 0;
+
+scrollBar.addEventListener('click', (event) => {
+  let top;
+  if (window.pageYOffset === 0) {
+    if (savedPageYOffset === 0) return;
+    top = savedPageYOffset;
+  } else {
+    savedPageYOffset = window.pageYOffset;
+    top = 0;
+  }
+  window.scrollTo({
+    top: top
+  });
+});
+
+chrome.storage.local.get({
+  width: 115,
+  color: '#dce2e8'
+}, (options) => {
+  scrollBar.style.setProperty('--width', options.width + 'px');
+  scrollBar.style.setProperty('--color', options.color);
+});
+
+function createScrollBar() {
+  const scrollBar = document.createElement('div');
+  const shadowRoot = scrollBar.attachShadow({
+    mode: 'closed'
+  });
+  const style = document.createElement('style');
+  style.textContent = `
+    :host {
+      background: transparent none repeat scroll 0% 0% !important;
+      border-radius: 0 !important;
+      border-style: none !important;
+      box-shadow: none !important;
+      cursor: pointer !important;
+      height: 100% !important;
+      margin: 0 !important;
+      opacity: 0.5 !important;
+      overflow: visible !important;
+      padding: 0 !important;
+      position: fixed !important;
+      right: 0 !important;
+      top: 0 !important;
+      width: var(--width, 115px) !important;
+      z-index: -1 !important;
+    }
+    :host(.over) {
+      background-color: var(--color, #dce2e8) !important;
+      z-index: 9999 !important;
+    }`;
+  shadowRoot.appendChild(style);
+  return scrollBar;
+}
