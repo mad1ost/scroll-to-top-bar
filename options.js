c7a6815f5a9ecea2e49803d190651b8354ab76ac
mad1ost <mad1ost@mail.ru>
2020-07-05 17:07:12 +0300

    use double quotes instead of single quotes

--- options.js
+++ options.js
@@ -1,12 +1,12 @@
-'use strict';
+"use strict";
 
-document.addEventListener('DOMContentLoaded', restoreOptions);
-document.querySelector("form").addEventListener('submit', saveOptions);
+document.addEventListener("DOMContentLoaded", restoreOptions);
+document.querySelector("form").addEventListener("submit", saveOptions);
 
 function restoreOptions() {
   chrome.storage.local.get({
     width: 115,
-    color: '#dce2e8'
+    color: "#dce2e8"
   }, (options) => {
     document.querySelector("#width").value = options.width;
     document.querySelector("#color").value = options.color;
