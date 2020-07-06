1d9b894a5ca7f9ec72f663e38b949162a1b1d732
mad1ost <mad1ost@mail.ru>
2020-07-06 21:53:20 +0300

    use single quotes instead of double quotes

--- options.js
+++ options.js
@@ -1,22 +1,22 @@
-"use strict";
+'use strict';
 
-document.addEventListener("DOMContentLoaded", restoreOptions);
-document.querySelector("form").addEventListener("submit", saveOptions);
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector('form').addEventListener('submit', saveOptions);
 
 function restoreOptions() {
   chrome.storage.local.get({
     width: 115,
-    color: "#dce2e8"
+    color: '#dce2e8'
   }, (options) => {
-    document.querySelector("#width").value = options.width;
-    document.querySelector("#color").value = options.color;
+    document.querySelector('#width').value = options.width;
+    document.querySelector('#color').value = options.color;
   });
 }
 
 function saveOptions(event) {
   chrome.storage.local.set({
-    width: document.querySelector("#width").value,
-    color: document.querySelector("#color").value
+    width: document.querySelector('#width').value,
+    color: document.querySelector('#color').value
   });
   event.preventDefault();
 }
