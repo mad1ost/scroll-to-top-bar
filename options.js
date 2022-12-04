c47303eca5d10122875bd0a26851d873ffd125e5
mad1ost <mad1ost@mail.ru>
2022-12-04 19:32:30 +0300

    use tabs instead of spaces

--- options.js
+++ options.js
@@ -4,19 +4,19 @@ document.addEventListener('DOMContentLoaded', restoreOptions);
 document.querySelector('form').addEventListener('submit', saveOptions);
 
 function restoreOptions() {
-  chrome.storage.local.get({
-    width: 115,
-    color: '#dce2e8'
-  }, (options) => {
-    document.querySelector('#width').value = options.width;
-    document.querySelector('#color').value = options.color;
-  });
+	chrome.storage.local.get({
+		width: 115,
+		color: '#dce2e8'
+	}, (options) => {
+		document.querySelector('#width').value = options.width;
+		document.querySelector('#color').value = options.color;
+	});
 }
 
 function saveOptions(event) {
-  chrome.storage.local.set({
-    width: document.querySelector('#width').value,
-    color: document.querySelector('#color').value
-  });
-  event.preventDefault();
+	chrome.storage.local.set({
+		width: document.querySelector('#width').value,
+		color: document.querySelector('#color').value
+	});
+	event.preventDefault();
 }
--- scroll-to-top-bar.js
+++ scroll-to-top-bar.js
@@ -1,80 +1,80 @@
 'use strict';
 
 chrome.storage.local.get({
-  width: 115,
-  color: '#dce2e8'
+	width: 115,
+	color: '#dce2e8'
 }, (options) => {
-  const scrollBar = createScrollBar(options);
-  document.body.append(scrollBar);
+	const scrollBar = createScrollBar(options);
+	document.body.append(scrollBar);
 
-  document.addEventListener('mouseover', (event) => {
-    if (event.target !== document.documentElement) return;
-    if (event.clientX >= document.documentElement.clientWidth) {
-      scrollBar.classList.add('over');
-      scrollBar.addEventListener('click', onClick);
-    }
-  });
+	document.addEventListener('mouseover', (event) => {
+		if (event.target !== document.documentElement) return;
+		if (event.clientX >= document.documentElement.clientWidth) {
+			scrollBar.classList.add('over');
+			scrollBar.addEventListener('click', onClick);
+		}
+	});
 
-  scrollBar.addEventListener('mouseleave', (event) => {
-    scrollBar.classList.remove('over');
-    scrollBar.removeEventListener('click', onClick);
-  });
+	scrollBar.addEventListener('mouseleave', (event) => {
+		scrollBar.classList.remove('over');
+		scrollBar.removeEventListener('click', onClick);
+	});
 
-  chrome.storage.onChanged.addListener((changes, area) => {
-    if (area !== 'local') return;
-    for (let key in changes) {
-      options[key] = changes[key].newValue;
-    }
-    scrollBar.style.setProperty('--width', options.width + 'px');
-    scrollBar.style.setProperty('--color', options.color);
-  });
+	chrome.storage.onChanged.addListener((changes, area) => {
+		if (area !== 'local') return;
+		for (let key in changes) {
+			options[key] = changes[key].newValue;
+		}
+		scrollBar.style.setProperty('--width', options.width + 'px');
+		scrollBar.style.setProperty('--color', options.color);
+	});
 
-  let prevY = 0;
+	let prevY = 0;
 
-  function onClick() {
-    let top;
-    if (window.scrollY === 0) {
-      if (prevY === 0) return;
-      top = prevY;
-    } else {
-      prevY = window.scrollY;
-      top = 0;
-    }
-    window.scrollTo({
-      top: top
-    });
-  }
+	function onClick() {
+		let top;
+		if (window.scrollY === 0) {
+			if (prevY === 0) return;
+			top = prevY;
+		} else {
+			prevY = window.scrollY;
+			top = 0;
+		}
+		window.scrollTo({
+			top: top
+		});
+	}
 
-  function createScrollBar(options) {
-    const scrollBar = document.createElement('div');
-    const shadowRoot = scrollBar.attachShadow({
-      mode: 'closed'
-    });
-    const style = document.createElement('style');
-    style.textContent = `
-      :host {
-        background: transparent none repeat scroll 0% 0% !important;
-        border-radius: 0 !important;
-        border-style: none !important;
-        box-shadow: none !important;
-        cursor: auto !important;
-        height: 100% !important;
-        margin: 0 !important;
-        opacity: 0.5 !important;
-        overflow: visible !important;
-        padding: 0 !important;
-        position: fixed !important;
-        right: 0 !important;
-        top: 0 !important;
-        width: var(--width, ${options.width}px) !important;
-        z-index: -1 !important;
-      }
-      :host(.over) {
-        background-color: var(--color, ${options.color}) !important;
-        cursor: pointer !important;
-        z-index: 9999 !important;
-      }`;
-    shadowRoot.append(style);
-    return scrollBar;
-  }
+	function createScrollBar(options) {
+		const scrollBar = document.createElement('div');
+		const shadowRoot = scrollBar.attachShadow({
+			mode: 'closed'
+		});
+		const style = document.createElement('style');
+		style.textContent = `
+			:host {
+				background: transparent none repeat scroll 0% 0% !important;
+				border-radius: 0 !important;
+				border-style: none !important;
+				box-shadow: none !important;
+				cursor: auto !important;
+				height: 100% !important;
+				margin: 0 !important;
+				opacity: 0.5 !important;
+				overflow: visible !important;
+				padding: 0 !important;
+				position: fixed !important;
+				right: 0 !important;
+				top: 0 !important;
+				width: var(--width, ${options.width}px) !important;
+				z-index: -1 !important;
+			}
+			:host(.over) {
+				background-color: var(--color, ${options.color}) !important;
+				cursor: pointer !important;
+				z-index: 9999 !important;
+			}`;
+		shadowRoot.append(style);
+		return scrollBar;
+	}
 });
